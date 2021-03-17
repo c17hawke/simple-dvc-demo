9d168b8eb47b42ad2664255313e648a52f28729a
c17hawke <2016peb5126@mnit.ac.in>
2021-03-17 20:14:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+create env 
+
+```bash
+conda create -n wineq python=3.7 -y
+```
+
+activate env
+```bash
+conda activate wineq
+```
+
+created a req file
+
+install the req
+```bash
+pip install -r requirements.txt
+```
+download the data from 
+
+https://drive.google.com/drive/folders/18zqQiCJVgF7uzXgfbIJ-04zgz1ItNfF5?usp=sharing
---
+++ template.py
@@ -0,0 +1,27 @@
+import os
+
+
+dirs = [
+    os.path.join("data", "raw"),
+    os.path.join("data","processed"),
+    "notebooks",
+    "saved_models",
+    "src"
+]
+
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f:
+        pass
+
+
+files = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src","__init__.py")
+]
+
+for file_ in files:
+    with open(file_, "w") as f:
+        pass
