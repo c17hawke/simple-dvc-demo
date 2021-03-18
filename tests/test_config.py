7306ea6241546f4ed3b8c18d3000db4109c457ff
c17hawke <2016peb5126@mnit.ac.in>
2021-03-18 20:44:23 +0530

    pytest example

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,4 +10,9 @@ def test_generic():
     with pytest.raises(NotInRange):
         if a not in range(10, 20):
             raise NotInRange
+
+def test_something():
+    a = 2
+    b = 2
+    assert True
         
