b6904d7181ca38ca724350bbc32cef892967c83d
c17hawke <2016peb5126@mnit.ac.in>
2021-03-18 20:18:39 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="c17hawke", 
+    packages=find_packages(),
+    license="MIT"
+)
+
---
+++ tests/test_config.py
@@ -0,0 +1,6 @@
+
+
+def test_generic():
+    a = 2
+    b = 2
+    assert a == b
