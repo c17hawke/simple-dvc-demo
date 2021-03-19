57c207636f5849876515326ac730423f12b671fa
c17hawke <2016peb5126@mnit.ac.in>
2021-03-19 20:10:02 +0530

    update app

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
 from flask import Flask, render_template, request, jsonify
 import os
-import yaml
-import joblib
 import numpy as np
 from prediction_service import prediction
+
 
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
     if request.method == "POST":
         try:
             if request.form:
-                data_req = dict(request.form)
-                response = prediction.form_response(data_req)
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
                 return render_template("index.html", response=response)
-
             elif request.json:
                 response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            # error = {"error": "Something went wrong!! Try again"}
-            return render_template("404.html", error=e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
+
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -4,18 +4,20 @@ import json
 import joblib
 import numpy as np
 
+
 params_path = "params.yaml"
 schema_path = os.path.join("prediction_service", "schema_in.json")
 
-class  NotInRange(Exception):
-    def __init__(self, message="Values entered are not in range"):
+class NotInRange(Exception):
+    def __init__(self, message="Values entered are not in expected range"):
         self.message = message
         super().__init__(self.message)
 
-class  NotInCols(Exception):
-    def __init__(self, message="Not in columns"):
+class NotInCols(Exception):
+    def __init__(self, message="Not in cols"):
         self.message = message
         super().__init__(self.message)
+
 
 
 def read_params(config_path=params_path):
@@ -28,7 +30,6 @@ def predict(data):
     model_dir_path = config["webapp_model_dir"]
     model = joblib.load(model_dir_path)
     prediction = model.predict(data).tolist()[0]
-
     try:
         if 3 <= prediction <= 8:
             return prediction
@@ -36,6 +37,7 @@ def predict(data):
             raise NotInRange
     except NotInRange:
         return "Unexpected result"
+
 
 def get_schema(schema_path=schema_path):
     with open(schema_path) as json_file:
@@ -48,9 +50,11 @@ def validate_input(dict_request):
         actual_cols = schema.keys()
         if col not in actual_cols:
             raise NotInCols
+
     def _validate_values(col, val):
         schema = get_schema()
-        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]):
+
+        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]) :
             raise NotInRange
 
     for col, val in dict_request.items():
@@ -59,13 +63,13 @@ def validate_input(dict_request):
     
     return True
 
+
 def form_response(dict_request):
     if validate_input(dict_request):
         data = dict_request.values()
         data = [list(map(float, data))]
         response = predict(data)
         return response
-
 
 def api_response(dict_request):
     try:
@@ -75,5 +79,5 @@ def api_response(dict_request):
             response = {"response": response}
             return response
     except Exception as e:
-        response = {"the_expected_range": get_schema(), "response": str(e)}
+        response = {"the_exected_range": get_schema(), "response": str(e) }
         return response
