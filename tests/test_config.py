10811d78667fa34a3e0cd2799090126f76076cd1
c17hawke <2016peb5126@mnit.ac.in>
2021-03-19 20:32:55 +0530

    test done

--- tests/test_config.py
+++ tests/test_config.py
@@ -62,3 +62,15 @@ def test_form_response_correct_range(data=input_data["correct_range"]):
 def test_api_response_correct_range(data=input_data["correct_range"]):
     res = api_response(data)
     assert  TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
+
+def test_form_response_incorrect_range(data=input_data["incorrect_range"]):
+    with pytest.raises(prediction_service.prediction.NotInRange):
+        res = form_response(data)
+
+def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInRange().message
+
+def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInCols().message
