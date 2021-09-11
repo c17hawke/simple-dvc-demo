e4fad2adbfeed9a202f8f675c27f6a50f2934976
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-11 22:14:41 +0530

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -15,7 +15,7 @@ def get_data(config_path):
     config = read_params(config_path)
     # print(config)
     data_path = config["data_source"]["s3_source"]
-    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
+    df = pd.read_csv(data_path, sep=";") # updated
     return df
 
 
